6c232e709d5caafbedab435ae346dca250cfc347
Orchard <orchard@cauldron.com>
2022-11-27 11:58:57 +1100

    did it

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -12,33 +12,35 @@ class Posts extends Component
     use WithPagination;
     use WithFileUploads;
 
-    public $title, $content, $featured_image;
+    public $title, $content, $featuredImage;
 
     protected $rules = [
             'title' => 'required|min:10,max:20',
             'content' => 'required', 
-            'featured_image' => 'image'    // validates image formats EG png, jpg, gif etc ... 
+            'featuredImage' => 'image'    // validates image formats EG png, jpg, gif etc ... 
         ];
         
     public function save()
     {
+        // dd($this->featuredImage);
         $data = [
             'title' => $this->title,
             'content' => $this->content,
             'user_id' => auth()->user()->id,
-            'featured_image' => $this->featured_image->hashName(),
+            'featuredImage' => $this->featuredImage->hashName(),
         ];
-
-        // dd($data['featured_image']);
-        // dd($this->featured_image);
         
         $this->validate();
 
-        if (!empty($this->featured_image)) {
-            $this->featured_image->store('public/images');
-        }
-
-        Post::create($data);
+       
+        $filename = $this->featuredImage->store('images');
+      
+        Post::create([
+            'title' => $this->title,
+            'content' => $this->content,
+            'user_id' => auth()->user()->id,
+            'featured_image' => $filename,
+        ]);
 
         $this->clearVars();
     }
@@ -52,6 +54,11 @@ class Posts extends Component
     {
         $this->title = null;
         $this->content = null;
+    }
+
+    public function updatedFeaturedImage()
+    {
+        // $this->validate(['featuredImage' => 'image|max:10']);
     }
 
     public function render()
--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,6 +1,8 @@
 <div>
     <x-input-label>Featured Image</x-input-label>
-    <input wire:model="featured_image" type="file" class=" shadow border-gray-300 focus:border-blue-300  focus:ring-blue-200 focus:ring-opacity-50 focus:ring-1"/>
+    <input wire:model="featuredImage" type="file" class=" shadow border-gray-300 focus:border-blue-300  focus:ring-blue-200 focus:ring-opacity-50 focus:ring-1"/>
+    <br>
+    @error('featuredImage') <span class="error text-red-500">*{{ $message }}</span> @enderror
     <x-input-label>Title</x-input-label>
     <input wire:model="title" type="text" class="w-full indent-6 rounded-md shadow border-gray-300 focus:border-blue-300  focus:ring-blue-200 focus:ring-opacity-50 focus:ring-1 " />
     @error('title') <span class="error text-red-500">*{{ $message }}</span> @enderror
@@ -17,12 +19,20 @@
             <div class="overflow-hidden">
               <table class="min-w-full">
                 <thead class="bg-white border-b">
+                <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Image</th>
                 <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Title</th>
                 <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Content</th>
             </thead>
             <tbody>
                 @foreach ($posts as $post)
                     <tr class="border-b border-gray-300 odd:bg-gray-200">
+                        <td>
+                            @if (!empty($post->featured_image))
+                                <img width="80px" src="{{ url('images/' . $post->featured_image) }}" alt="" />
+                            @else
+                                No image available
+                            @endif
+                        </td>
                         <td class="py-2 px-2">{{ $post->title }}</td>
                         <td>{{ $post->content }}</td>
                         <td class="text-right"><button wire:click="delete({{ $post->id }})" class="text-white bg-red-700 hover:bg-red-800 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1.5 mr-2 mb-dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none  dark:focus:ring-red-800 mr-5
